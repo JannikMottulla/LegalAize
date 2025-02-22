7e812a64ddce9659f0925aedfb6ef8d5e5404ae4
Jannik Mottulla <jannikmottulla@gmail.com>
2025-02-22 06:00:05 +0100

    tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/LegalAize",
+  base: "/LegalAize/",
   plugins: [react(), tailwindcss()],
 });
